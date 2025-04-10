87a8c1c64f89d0d98f41e038b1473029126611c3
Pavithra <pavis@PAVITHRA.com>
2025-04-10 17:35:56 +0530

    Fix nested form issue and update backend logic

--- resume_web_app/app.py
+++ resume_web_app/app.py
@@ -13,8 +13,9 @@ app.secret_key = "some_secret_key"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_email@gmail.com'
-app.config['MAIL_PASSWORD'] = 'your_app_password'
+import os
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = 'your_email@gmail.com'
 mail = Mail(app)
 
@@ -158,4 +159,3 @@ ResumeMatchr Team"""
 
 if __name__ == '__main__':
     app.run(debug=True)
- 
